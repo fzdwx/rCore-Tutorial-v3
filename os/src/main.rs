a5b5493507aa56108096689a01ad7cf4f67053da
fzdwx <likelovec@gmail.com>
2023-01-01 17:44:22 +0800

    init_logging

--- os/src/main.rs
+++ os/src/main.rs
@@ -4,14 +4,16 @@
 
 #[macro_use]
 mod console;
+mod batch;
 mod lang_items;
+mod logging;
 mod sbi;
+mod sync;
 mod syscall;
 mod trap;
-mod batch;
-mod sync;
 
 use core::arch::global_asm;
+use log::info;
 
 global_asm!(include_str!("entry.asm"));
 global_asm!(include_str!("link_app.S"));
@@ -22,17 +24,16 @@ fn clear_bss() {
         fn ebss();
     }
     unsafe {
-        core::slice::from_raw_parts_mut(
-            sbss as usize as *mut u8, 
-            ebss as usize - sbss as usize,
-        ).fill(0);
+        core::slice::from_raw_parts_mut(sbss as usize as *mut u8, ebss as usize - sbss as usize)
+            .fill(0);
     }
 }
 
 #[no_mangle]
 pub fn rust_main() -> ! {
     clear_bss();
-    println!("[kernel] Hello, world!");
+    logging::init().unwrap();
+    info!("[kernel] Hello, world!");
     trap::init();
     batch::init();
     batch::run_next_app();
